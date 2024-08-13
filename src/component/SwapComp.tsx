a0558a96351cd080314f065477bcf06e5e81a841
Umorenism <umorenismvic@gmail.com>
2024-08-13 00:52:03 -0700

    updated

--- src/component/SwapComp.tsx
+++ src/component/SwapComp.tsx
@@ -1,7 +1,8 @@
-import { FaWallet, FaAngleRight } from "react-icons/fa6";
+import { FaWallet } from "react-icons/fa6";
+import TypingEffect from 'react-typing-effect';
 
 import {
-  RiArrowDropDownLine,
+  
   RiLoader4Fill,
   RiEqualFill,
 } from "react-icons/ri";
@@ -10,13 +11,10 @@ import { CgSwap } from "react-icons/cg";
 import { useState } from "react";
 
 export const SwapComp = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(null); // Track which dropdown is open
+ 
   const [isLoading, setIsLoading] = useState(false);
 
-  const toggleDropdown = (dropdown: any) => {
-    setDropdownOpen(dropdownOpen === dropdown ? null : dropdown); // Toggle dropdown
-  };
-
+  
   const handleContinue = () => {
     setIsLoading(true);
     setTimeout(() => {
@@ -27,139 +25,65 @@ export const SwapComp = () => {
   return (
     <div className="min-h-screen bg-slate-950 py-16 text-white relative">
       <div className="text-center py-2">
-        <h1 className="text-2xl">SWAP</h1>
+      <h1 className="text-md mt-4 text-[#1DD55E]">
+          <TypingEffect 
+            text={["SWAP your coin"]}
+            speed={100}
+            eraseSpeed={50}
+            eraseDelay={1000}
+            typingDelay={500}
+            
+          />
+        </h1>
       </div>
       <hr className="border-gray-600" />
       <div className="mt-10 px-4">
-        <div className="bg-gray-700 p-6 text-white rounded-lg relative mb-4">
-          <div className="flex justify-between items-center mb-4">
-            <div className="flex items-center">
-              From BNB Smart Chain{" "}
-              <RiArrowDropDownLine
-                size={30}
-                onClick={() => toggleDropdown("from")}
-                className="cursor-pointer"
-              />
-            </div>
-            <div className="flex items-center gap-1">
-              <FaWallet className="text-gray-100" />
-              <span className="text-[#1DD55E]">0</span>
-            </div>
-          </div>
-          {dropdownOpen === "from" && (
-            <div className="absolute top-16 left-0 right-0 bg-white py-4 p-4 rounded-lg shadow-lg text-slate-500 z-20">
-              <div className="flex flex-col gap-2">
-                <div className="bg-slate-200 p-2 rounded-lg flex justify-between">
-                  <h1 className="text-[#1DD55E] font-bold">BNB</h1>
-                  <p>0.00002</p>
-                </div>
-                <div className="bg-slate-400 p-2 rounded-lg flex justify-between">
-                  <h1 className="text-[#1DD55E] font-bold">DONS</h1>
-                  <p>0.000</p>
-                </div>
-              </div>
-            </div>
-          )}
-          <div className="flex justify-between items-center mb-4">
-            <div className="flex items-center text-xl font-bold">
-              BNB{" "}
-              <FaAngleRight
-                onClick={() => toggleDropdown("fromDetails")}
-                className="cursor-pointer"
-              />
-            </div>
-            {dropdownOpen === "fromDetails" && (
-              <div className="absolute top-16 left-0 right-0 bg-white py-4 p-4 rounded-lg shadow-lg text-slate-500 z-20">
-                <div className="flex flex-col gap-2">
-                  <div className="bg-slate-200 p-2 rounded-lg flex justify-between">
-                    <h1 className="text-[#1DD55E]font-bold">BNB</h1>
-                    <p>0.00002</p>
-                  </div>
-                  <div className="bg-slate-400 p-2 rounded-lg flex justify-between">
-                    <h1 className="text-[#1DD55E]font-bold">DONS</h1>
-                    <p>0.000</p>
-                  </div>
-                </div>
-              </div>
-            )}
-            <p>
-              0 <span className="text-[#1DD55E] cursor-pointer">Max</span>
-            </p>
-          </div>
-          <div className="absolute top-[260px] left-1/2 transform -translate-x-1/2 bg-[#1DD55E] rounded-full h-10 w-10 flex items-center justify-center z-30">
-            <LuArrowDownUp size={25} className="text-white" />
+        <div className="bg-gray-700 px-2 py-4 text-white rounded-lg relative ">
+          <div className="flex justify-between items-center gap-2 mb-4">
+
+          <select name="" id="" className="py-2" style={
+            {backgroundColor:"transparent",fontSize:15,width:"80%",border:"none",color:""}
+          }>
+            <option selected disabled>From BNB Smart Chain</option>
+            <option value="Bnb">From BNB Smart Chain</option>
+            <option selected disabled>From BNB Smart Chain</option>
+          </select>
+          <div className="flex items-center gap-1">
+          <FaWallet size={14} className="cursor-pointer"
+              /><span className="text-[#1DD55E]">0</span>
+          </div> 
           </div>
         </div>
-
-        <div className="bg-gray-700 p-6 text-white rounded-lg relative mb-4">
-          <div className="flex justify-between items-center mb-4">
-            <div className="flex items-center">
-              To BNB Smart Chain{" "}
-              <RiArrowDropDownLine
-                size={30}
-                onClick={() => toggleDropdown("to")}
-                className="cursor-pointer"
-              />
-            </div>
-            <div className="flex items-center gap-1">
-              <FaWallet className="text-gray-100" />
-              <span className="text-[#1DD55E]">0</span>
-            </div>
-          </div>
-          {dropdownOpen === "to" && (
-            <div className="absolute top-16 left-0 right-0 bg-white py-4 p-4 rounded-lg shadow-lg text-slate-500 z-20">
-              <div className="flex flex-col gap-2">
-                <div className="bg-slate-200 p-2 rounded-lg flex justify-between">
-                  <h1 className="text-[#1DD55E] font-bold">BNB</h1>
-                  <p>0.00002</p>
-                </div>
-                <div className="bg-slate-400 p-2 rounded-lg flex justify-between">
-                  <h1 className="text-[#1DD55E] font-bold">DONS</h1>
-                  <p>0.000</p>
-                </div>
-              </div>
-            </div>
-          )}
-          <div className="flex justify-between items-center mb-4">
-            <div className="flex items-center text-xl font-bold">
-              BNB{" "}
-              <FaAngleRight
-                onClick={() => toggleDropdown("toDetails")}
-                className="cursor-pointer"
-              />
-            </div>
-            {dropdownOpen === "toDetails" && (
-              <div className="absolute top-16 left-0 right-0 bg-white py-4 p-4 rounded-lg shadow-lg text-slate-500 z-20">
-                <div className="flex flex-col gap-2">
-                  <div className="bg-slate-200 p-2 rounded-lg flex justify-between">
-                    <h1 className="text-[#1DD55E]font-bold">BNB</h1>
-                    <p>0.00002</p>
-                  </div>
-                  <div className="bg-slate-400 p-2 rounded-lg flex justify-between">
-                    <h1 className="text-[#1DD55E] font-bold">DONS</h1>
-                    <p>0.000</p>
-                  </div>
-                </div>
-              </div>
-            )}
-            <p>
-              0 <span className="text-[#1DD55E] cursor-pointer">Max</span>
-            </p>
-          </div>
-        </div>
-
-        <div className="bg-gray-700 p-4 text-white rounded-lg mb-4">
-          <div className="flex justify-center items-center text-xl">
+<div className="flex justify-center">
+  <p className="text-center rounded-full h-8 w-8 bg-[#1DD55E] flex items-center justify-center"><LuArrowDownUp size={16} className="text-white" /></p>
+</div>
+<div className="bg-gray-700 px-2 py-4 text-white rounded-lg relative mb-4">
+<div className="flex justify-between  gap-2 mb-4">
+<select name="" id="" className="py-2" style={
+  {backgroundColor:"transparent",fontSize:15,width:"80%",border:"1px",color:""}
+}>
+  <option selected disabled>To BNB Smart Chain</option>
+  <option value="Bnb">To BNB Smart Chain</option>
+  <option selected disabled>From BNB Smart Chain</option>
+</select>
+<div className="flex items-center gap-1">
+<FaWallet size={14} className="cursor-pointer"
+    /><span className="text-[#1DD55E]">0</span>
+</div> 
+</div>
+</div>
+        <div className="p-4 bg-gray-700 text-white rounded-lg mb-4">
+          <div className="flex justify-between items-center text-sm">
             <RiLoader4Fill className="mr-2" />
-            1BNB <RiEqualFill className="mx-2" />
+            1BNB <RiEqualFill className="mx-4" />
             7.227.055.535427 DONS
-            <CgSwap className="ml-2" />
+            <CgSwap size={25} className="ml-4" />
           </div>
         </div>
 
         <button
           onClick={handleContinue}
-          className="w-full py-4 rounded-xl text-white bg-[#1DD55E] text-xl transition-transform transform hover:scale-105"
+          className="w-full py-2 rounded-xl text-white bg-[#1DD55E] text-xl transition-transform transform hover:scale-105"
           disabled={isLoading}
         >
           {isLoading ? (
